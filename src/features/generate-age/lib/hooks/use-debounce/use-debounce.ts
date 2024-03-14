441fa042cfc9a5fdd406e1318bbb9efc56ec015f
Ani-21 <kvikviniya.am@edu.spbstu.ru>
2024-03-14 19:28:20 +0300

    feat: add debounce hook

---
+++ src/features/generate-age/lib/hooks/use-debounce/use-debounce.ts
@@ -0,0 +1,36 @@
+import { useEffect } from "react";
+
+function debounce<A = unknown, R = void>(
+  fn: (args: A) => R,
+  delay: number,
+): [(args: A) => Promise<R>, () => void] {
+  let timer: number | undefined;
+
+  const debouncedFn = (args: A): Promise<R> =>
+    new Promise((resolve) => {
+      if (timer) {
+        clearTimeout(timer);
+      }
+
+      timer = setTimeout(() => {
+        resolve(fn(args));
+      }, delay);
+    });
+
+  const teardown = () => clearTimeout(timer);
+
+  return [debouncedFn, teardown];
+}
+
+export const useDebounce = <A = unknown, R = void>(
+  fn: (args: A) => R,
+  delay: number,
+): ((args: A) => Promise<R>) => {
+  const [debouncedFn, teardown] = debounce<A, R>(fn, delay);
+
+  useEffect(() => {
+    () => teardown();
+  });
+
+  return debouncedFn;
+};
--- src/features/generate-age/ui/generate-age.tsx
+++ src/features/generate-age/ui/generate-age.tsx
@@ -1,7 +1,11 @@
+import { useMutation } from "@tanstack/react-query";
 import { Icon16Clear } from "@vkontakte/icons";
 import { Button, FormItem, IconButton, Input } from "@vkontakte/vkui";
+import React from "react";
 import { Controller, useForm } from "react-hook-form";
 
+import { fetchAgeByName } from "..";
+import { useDebounce } from "../lib/hooks/use-debounce/use-debounce";
 import resolver from "../lib/resolver/age-resolver";
 import { AgeFormSchema } from "../model/types/age-schema.types";
 
@@ -11,7 +15,16 @@ type GenerateAgeProps = {
 
 const GenerateAge = (props: GenerateAgeProps) => {
   const { onSubmit } = props;
-  const { handleSubmit, control, reset, formState } = useForm<AgeFormSchema>({ resolver });
+  const mutation = useMutation({ mutationFn: fetchAgeByName });
+  const { handleSubmit, control, reset, formState, setValue } = useForm<AgeFormSchema>({
+    resolver,
+  });
+  const debouncedFn = useDebounce(mutation.mutate, 3000);
+
+  const inputChanged = (field: keyof AgeFormSchema, e: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(field, e.target.value);
+    e.target.value && debouncedFn(e.target.value).then((res) => console.log(res));
+  };
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
@@ -22,9 +35,9 @@ const GenerateAge = (props: GenerateAgeProps) => {
         <Controller
           name="name"
           control={control}
-          render={({ field: { onChange } }) => (
+          render={({ field: { name } }) => (
             <Input
-              onChange={onChange}
+              onChange={(e) => inputChanged(name, e)}
               status={formState.errors.name ? "error" : "default"}
               after={
                 <IconButton hoverMode="opacity" label="Очистить поле" onClick={() => reset()}>
